2c83fe70d482cac59acc832b6fa1c9b8c8abe201
[marianorp] <[mariano.rperez26@gmail.com]>
2022-10-23 15:35:05 -0300

    ✨Add form Contact✨

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,47 @@
-// import styles from "./styles/Form.module.css"
+import styles from "./styles/Form.module.css"
 
 
-// function Form() {
-//     return (
+function Form() {
+    return (
+
+    <div className={styles.container}>
+
+        <div className={styles.form_container}>
+        
+            <form id="form" class="form">
+
+                <div className={styles.control}>
+                    <label for="username">Name</label>
+                    <input type="text" placeholder="John Doe" id="name" />
+                    <i class="check"></i>
+                    <i class="wrong"></i>
+                    <small>Error message</small>
+                </div>
+
+                <div className={styles.control}>
+                    <label for="username">Email</label>
+                    <input type="email" placeholder="example@gmail.com" id="email"/>
+                    <i class="check"></i>
+                    <i class="wrong"></i>
+                    <small>Error message</small>
+                </div>
+
+                <div className={styles.control}>
+                    <label for="username">Message</label>
+                    <textarea type="message" placeholder="Write your message here" id="message"></textarea>
+                    <i class="check"></i>
+                    <i class="wrong"></i>
+                    <small>Error message</small>
+                </div>
+                
+                <button onclick="getData()">Submit</button>
+            </form> 
+        </div>
+
+    </div>
+
        
-//     );
-//   }
+    );
+  }
   
-//   export default Form;
+  export default Form;
