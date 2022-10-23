a4edb52d7ebee6a8e84a2f2bd78229e59e3cf28b
[marianorp] <[mariano.rperez26@gmail.com]>
2022-10-23 15:35:34 -0300

    Add form contact✨

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -3,33 +3,43 @@ import Cards from "./Cards";
 import styles from "./styles/Api.module.css";
 
 
+
 function Api() {
 
   const [data, setData] = useState([])
 
   useEffect(() => {
 
-    const rickAndMorty = fetch("https://rickandmortyapi.com/api/character")
+    const api = fetch("https://rickandmortyapi.com/api/character")
     
-    rickAndMorty
+    api
 
       .then((data) => data.json())
       .then((data) => setData(data.results))
-  }, [])
+    
+    }, 
+  [])
 
 
   return (
+
     <div className={styles.container}>
       <div className={styles.title}>
           <h1>Rick and Morty Cards</h1>
-        </div>
-        <div className="cards">
-          {data.length !== 0 ? data.map((e) => <Cards image={e.image} name={e.name} status={e.status} species={e.species} gender={e.gender} origin={e.origin} location={e.location} />) : null}
-        </div>   
-        {/* <div class="arrow-button">
-            <a href=""><span class="fas fa-angle-up"></span></a>
-        </div> */}
-
+      </div>
+      <div className="cards">
+          {data.length !== 0 ? data.map((e) => 
+            <Cards 
+              image={e.image} 
+              name={e.name} 
+              status={e.status} 
+              species={e.species} 
+              gender={e.gender} 
+              origin={e.origin} 
+              location={e.location} 
+            />) : 
+          null}
+      </div>   
     </div>
 
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,14 +6,12 @@ function Cards({image, name, species, status, gender, origin, location}) {
        
         <div className={styles.one_card}>
             <img src={image} alt=""/>
-            
                 <h3>{name}</h3>
                 <h3>Specie: {species}</h3>
                 <h3>Status: {status}</h3>
                 <h3>Gender: {gender}</h3>
                 <h3>Origen: {origin?.name}</h3>
                 <h3>Location: {location?.name}</h3> 
-
         </div> 
     );
   }
